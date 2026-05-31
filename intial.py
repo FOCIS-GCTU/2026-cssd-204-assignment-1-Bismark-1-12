d9e3d2b81154be671ba5325e39339a7aefa8bad1
Bismark-1-12 <goddaughteraudrey008@gmail.com>
2026-05-31 23:29:37 +0000

    Add files via upload

---
+++ intial.py
@@ -0,0 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Bismark Anakwa Kwadwo
+# STUDENT ID: 2425403907
+# Email: 2425403907@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Bismark Anakwa Kwadwo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    print()
+    print('...BAK')
+    print()
+
+    print('...BBBBBBBB..............AAAAAA............KK........KK........')
+    print('...BB.......BB..........AA....AA...........KK.......KK.........')
+    print('...BB........BB........AA......AA..........KK.....KK...........')
+    print('...BB.......BB.........AAAAAAAAAAA.........KK...KK.............')
+    print('...BBBBBBBBBBB........AA.........AA........KK..KK..............')
+    print('...BB........BB.......AA.........AA........KKKK................')
+    print('...BB........BB.......AA.........AA........KK..KK..............')
+    print('...BB........BB.......AA..........AA.......KK....KK............')
+    print('...BB.......BB..**....AA..........AA..**...KK......KK.....**...')
+    print('...BBBBBBBB.....**....AA..........AA..**...KK........KK...**...')
+    print()
+
+
+main()
